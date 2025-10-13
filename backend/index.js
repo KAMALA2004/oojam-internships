b15cb0f23221b5292cefcf70d66ac0e1c046acea
Kamala Vasanthi S <119476054+KAMALA2004@users.noreply.github.com>
2025-10-13 18:37:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+
 const authRoutes = require('./routes/auth');
+const internshipRoutes = require('./routes/internship');
 
 const app = express();
-app.use(cors());
+console.log('internshipRoutes:', internshipRoutes);
+
+app.use(cors({
+  origin: 'http://localhost:3000', // frontend
+  credentials: true,               // allow cookies
+}));
 app.use(express.json());
 
 app.use('/api', authRoutes);
+app.use('/api/internship', internshipRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => {
